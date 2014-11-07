27447e851480da433f46a2e079dfe5c5da9280f8
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:28:13 +0100

    Cleanup tests

--- tests/test_party_social_contact.py
+++ tests/test_party_social_contact.py
@@ -7,24 +7,16 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class PartySocialContactTestCase(unittest.TestCase):
-    '''
-    Test Party Social Contact module.
-    '''
+    'Test Party Social Contact module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('party_social_contact')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
