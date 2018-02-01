aa3c16564672f5ed1c2bc1b86f290c8701e990c2
C?dric Krier <ced@b2ck.com>
2018-02-01 14:18:00 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .contact_mechanism import *
+from . import contact_mechanism
 
 
 def register():
     Pool.register(
-        ContactMechanism,
+        contact_mechanism.ContactMechanism,
         module='party_social_contact', type_='model')
